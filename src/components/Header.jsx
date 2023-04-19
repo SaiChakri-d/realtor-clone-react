1dc0eb857219083de3bca6f7eecb5c31c4e162f5
Sai Chakri D <98957092+SaiChakri-d@users.noreply.github.com>
2023-04-19 09:30:02 +0530

    updated the components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <Header>
+            <div>
+                <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" />
+            </div>
+        </Header>
+    </div>
+  )
+}
