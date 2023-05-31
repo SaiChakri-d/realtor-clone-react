2a9af4145cf41b63e27abd8962657326d89a4b51
Sai Chakri D <98957092+SaiChakri-d@users.noreply.github.com>
2023-05-31 16:58:32 +0530

    slider modified

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -67,10 +67,10 @@ export default function Slider() {
                 }}
                 className="relative w-full h-[300px] overflow-hidden"
               ></div>
-              <p className="text-[#f1faee] absolute left-1 top-3 font-medium max-w-[90%] bg-[#457b9d] shadow-lg opacity-90 p-2 rounded-br-2xl">
+              <p className="text-[#f1faee] absolute left-1 top-3 font-medium max-w-[90%] bg-[#457b9d] shadow-lg opacity-90 p-2 rounded-br-3xl">
                 {data.name}
               </p>
-              <p className="text-[#f1faee] absolute left-1 bottom-1 font-semibold max-w-[90%] bg-[#e63946] shadow-lg opacity-90 p-2 rounded-tr-2xl">
+              <p className="text-[#f1faee] absolute left-1 bottom-1 font-semibold max-w-[90%] bg-[#e63946] shadow-lg opacity-90 p-2 rounded-tr-3xl">
                 ${data.discountedPrice ?? data.regularPrice}
                 {data.type === "rent" && " / month"}
               </p>
